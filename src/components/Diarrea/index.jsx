03850b82966e4f23770479ed8283dea69614d092
Miguel Rivera <mariveran@unal.edu.co>
2020-12-22 10:57:17 -0500

    initial commit

---
+++ src/components/Diarrea/index.jsx
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+
+
+export default class Diarrea extends Component {
+    render(){
+        return(
+            <div>
+                <p>This page works!!!</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/Laxium/index.jsx
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+
+
+export default class Laxium extends Component {
+    render(){
+        return(
+            <div>
+                <p>This page works!!!</p>
+            </div>
+        );
+    }
+}
